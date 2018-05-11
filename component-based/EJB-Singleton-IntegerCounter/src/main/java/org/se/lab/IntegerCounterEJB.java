1a33496532fc44b1d4ba4d8b1fcd45802d9c40f1
teiniker <egon.teiniker@gmail.com>
2018-05-11 18:13:13 +0200

    Refactoring: Changes concurrency policy.

--- component-based/EJB-Singleton-IntegerCounter/src/main/java/org/se/lab/IntegerCounterEJB.java
+++ component-based/EJB-Singleton-IntegerCounter/src/main/java/org/se/lab/IntegerCounterEJB.java
@@ -2,6 +2,8 @@ package org.se.lab;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.ejb.ConcurrencyManagement;
+import javax.ejb.ConcurrencyManagementType;
 import javax.ejb.Remote;
 import javax.ejb.Singleton;
 
@@ -9,7 +11,7 @@ import org.jboss.logging.Logger;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
-
+@ConcurrencyManagement(ConcurrencyManagementType.BEAN)
 @Singleton
 @Remote(IntegerCounter.class)
 public class IntegerCounterEJB 
